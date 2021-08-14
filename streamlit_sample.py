62a6450373429d840f3748b9231263604160517f
kunishou <69422183+kunishou@users.noreply.github.com>
2021-08-14 23:49:13 +0900

    Update streamlit_sample.py

--- streamlit_sample.py
+++ streamlit_sample.py
@@ -5,30 +5,3 @@ st.title('Widget Sample')
 #image = Image.open('sample.png')
 #st.image(image,use_column_width=True)
 #st.write('')
-
-# ボタン
-st.markdown('## ボタン')
-st.write("st.button('ラベル')")
-button = st.button('ラベル')
-
-if button:
-    st.write("ボタンが押下されました")
-
-st.write('')
-
-# チェックボックス
-st.markdown('## チェックボックス')
-st.write("st.checkbox('ラベル')")
-check = st.checkbox('ラベル')
-
-if check:
-    st.write("ボックスにチェックがされました")
-
-st.write('')
-
-# ラジオボタン
-st.markdown('## ラジオボタン')
-st.write("st.radio('ラベル', ('選択肢1', '選択肢2', '選択肢3'))")
-radio = st.radio("ラベル",('選択肢1', '選択肢2', '選択肢3'))
-st.write(radio + 'が選択されています')
-st.write('')
